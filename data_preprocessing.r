ac954f60b4bf3edccb28fe04422d05328d127f28
Tata Coa <35470096+tatacoa@users.noreply.github.com>
2019-01-08 18:12:18 +0100

    pre-processing

---
+++ data_preprocessing.r
@@ -0,0 +1,80 @@
+#ML2 Project
+# @Autor: Ana Maria Sandoval 
+
+
+#Kaggle
+# https://www.kaggle.com/jonathanbouchet/pokemon-battles/data
+# https://www.kaggle.com/nayansolanki2411/world-of-pokemon
+# https://www.kaggle.com/mmetter/pokemon-data-analysis-tutorial
+# https://www.kaggle.com/rautaki0127/pokemon-data-science-challenge
+
+#Libraries
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
+#install.packages('corrplot')
+library(corrplot)
+#install.packages('caret')
+library(caret)
+#install.packages('ggthemes')
+library(ggthemes)
+library(RColorBrewer)
+#install.packages('fmsb')
+library(fmsb)
+#install.packages('rpart.plot')
+library(rpart.plot)
+#install.packages('ROCR')
+library(ROCR)
+library(tidyverse)
+library(ggplot2)
+
+# Load data
+setwd("~/Pokemon")
+pokemon <- read.csv('./Data/pokemon.csv')
+fights <- read.csv('./Data/combats.csv')
+
+#EDV PLots
+par(las=2)
+barplot(table(pokemon$Type.1), 
+        col = heat.colors(length(unique(pokemon$Type.1))), 
+        main = 'Type 1 Pokemon')
+barplot(table(pokemon$Type.2), 
+        col = heat.colors(length(unique(pokemon$Type.2))), 
+        main = 'Type 2 Pokemon')
+barplot(table(pokemon$Generation), 
+        col = heat.colors(length(unique(pokemon$Generation))),
+        main = 'Generation Pokemon')
+                              
+
+boxplot(Defense ~ Type.1, data = pokemon, 
+        col = rainbow(length(unique(pokemon$Type.1))))
+boxplot(Defense ~ Type.1,
+        data=pokemon,
+        main="Defense Range by Pokemon Type",
+        xlab="Type 1",
+        ylab="Defense",
+        col="orange",
+        border="brown"
+)
+                                  
+#nullvalues
+colSums(is.na(pokemon))
+colSums(is.na(fights))
+
+dim(pokemon)
+head(pokemon)
+colnames(pokemon)
+
+dim(fights)
+head(fights)
+colnames(fights)
+
+names <- pokemon[,c(1,2)]; head(names) # join id with pokemon name
+colnames(names)
+head(pokemon)
+
+#Map the figths table from id to pokemon name
+fights <- data.frame(lapply(fights, function(x) names$Name[match(x,names$X.)]))
+head(fights)
+
+
