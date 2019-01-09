457dd2e9d17ef110afe36c5f61e23d4817d1f875
Shuaib Yunus <shuaibiyy@gmail.com>
2019-01-10 00:57:17 +0100

    Implement preprocessing of fight pairs

--- data_preprocessing.r
+++ data_preprocessing.r
@@ -29,9 +29,9 @@ library(tidyverse)
 library(ggplot2)
 
 # Load data
-setwd("~/Pokemon")
-pokemon <- read.csv('./Data/pokemon.csv')
-fights <- read.csv('./Data/combats.csv')
+setwd("/Users/yunussh/playground/Pokemon-winner-prediction")
+pokemon <- read.csv('./pokemon.csv')
+fights <- read.csv('./combats.csv')
 
 #EDV PLots
 par(las=2)
@@ -74,7 +74,57 @@ colnames(names)
 head(pokemon)
 
 #Map the figths table from id to pokemon name
-fights <- data.frame(lapply(fights, function(x) names$Name[match(x,names$X.)]))
-head(fights)
+fights.name <- data.frame(lapply(fights, function(x) names$Name[match(x,names$X.)]))
+head(fights.name)
 
+get_win_table <- function() {
+  counts <- group_by(fights.name, Winner)
+  count_table <- summarise(counts, count = n())
+  return(count_table)
+}
 
+get_firsts_table <- function() {
+  counts <- group_by(fights.name, First_pokemon)
+  count_table <- summarise(counts, count = n())
+  return(count_table)
+}
+
+get_seconds_table <- function() {
+  counts <- group_by(fights.name, Second_pokemon)
+  count_table <- summarise(counts, count = n())
+  return(count_table)
+}
+
+win_table <- get_win_table()
+firsts_table <- get_firsts_table()
+seconds_table <- get_seconds_table()
+
+win_counts <- sapply(pokemon$Name, function(x) win_table$count[match(x,win_table$Winner)])
+first_counts <- sapply(pokemon$Name, function(x) firsts_table$count[match(x,firsts_table$First_pokemon)])
+second_counts <- sapply(pokemon$Name, function(x) seconds_table$count[match(x,seconds_table$Second_pokemon)])
+
+pokemon_feats <- cbind(pokemon, win_counts, first_counts, second_counts)
+pokemon_feats$losses <- pokemon_feats$first_counts + pokemon_feats$second_counts - pokemon_feats$win_counts
+
+pokemon_feats$win_ratio <- pokemon_feats$win_counts / (pokemon_feats$second_counts + pokemon_feats$first_counts)
+head(pokemon_feats)
+
+write.csv(pokemon_feats, file="./features.csv")
+
+for (i in 1:nrow(pokemon_feats)) {
+ i_name <- pokemon_feats[i,2]
+ 
+ for (j in 1:nrow(names)) {
+   j_name <- names[j,2]
+   
+   matches <- fights.name[(fights.name$First_pokemon == i_name & fights.name$Second_pokemon == j_name) |
+              (fights.name$First_pokemon == j_name & fights.name$Second_pokemon == i_name), ]
+   w <- nrow(matches[matches$Winner == i_name,])
+   l <- nrow(matches) - w
+   
+   pokemon_feats[i, paste("wins", j, sep = "_")] <- w
+   pokemon_feats[i, paste("losses", j, sep = "_")] <- l
+ }
+}
+
+write.csv(pokemon_feats, file="./features_with_fight_pairs.csv")
