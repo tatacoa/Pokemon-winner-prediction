621d303d8712712b0746ac1aa533e041c0296214
agademic <arthur.gogohia@gmail.com>
2019-01-10 12:35:14 +0100

    changes ins code structure, plots and exploration added

--- data_preprocessing.r
+++ data_preprocessing.r
@@ -20,7 +20,7 @@ library(caret)
 library(ggthemes)
 library(RColorBrewer)
 #install.packages('fmsb')
-library(fmsb)
+library(fmsb) # radarchart
 #install.packages('rpart.plot')
 library(rpart.plot)
 #install.packages('ROCR')
@@ -29,12 +29,28 @@ library(ROCR)
 #library(tidyverse)
 library(ggplot2)
 
+##### DATA IMPORT #####
 # Load data
-setwd("~/Documents/Pokemon-winner-prediction")
-pokemon <- read.csv('./pokemon.csv')
-fights <- read.csv('./combats.csv')
+setwd("Pokemon-winner-prediction")
+pokemon <- read.csv('data/pokemon.csv')
+fights <- read.csv('data/combats.csv')
 
-#EDV PLots
+##### EXPLORATY DATA ANALYSIS #####
+# data exploration
+dim(pokemon)
+names(pokemon)
+head(pokemon)
+colSums(is.na(pokemon)) # no missing values
+summary(pokemon)
+
+dim(fights)
+names(fights)
+head(pokemon)
+colSums(is.na(fights))
+wins <- group_by(fights, Winner)
+summarise(wins, count = n())
+
+# visualization
 par(las=2)
 barplot(table(pokemon$Type.1), 
         col = heat.colors(length(unique(pokemon$Type.1))), 
@@ -45,10 +61,26 @@ barplot(table(pokemon$Type.2),
 barplot(table(pokemon$Generation), 
         col = heat.colors(length(unique(pokemon$Generation))),
         main = 'Generation Pokemon')
-                              
 
-boxplot(Defense ~ Type.1, data = pokemon, 
-        col = rainbow(length(unique(pokemon$Type.1))))
+# plot specific feature distribution by Pokemon type
+boxplot(HP ~ Type.1,
+        data=pokemon,
+        main="Health points Range by Pokemon Type",
+        xlab="Type 1",
+        ylab="Health points",
+        col="grey",
+        border="brown"
+)
+
+boxplot(Attack ~ Type.1,
+        data=pokemon,
+        main="Attack Range by Pokemon Type",
+        xlab="Type 1",
+        ylab="Attack",
+        col="red",
+        border="brown"
+)
+
 boxplot(Defense ~ Type.1,
         data=pokemon,
         main="Defense Range by Pokemon Type",
@@ -57,18 +89,33 @@ boxplot(Defense ~ Type.1,
         col="orange",
         border="brown"
 )
-                                  
-#nullvalues
-colSums(is.na(pokemon))
-colSums(is.na(fights))
 
-dim(pokemon)
+# set color for each pokemon type for plotting
+color<-c("#6F35FC","#B7B7CE","#A98FF3","#F95587","#B6A136","#EE8130","#F7D02C","#705746","#735797","#E2BF65","#96D9D6","#6390F0","#7AC74C","#C22E28","#D685AD","#A33EA1","#A8A77A","#A6B91A")
+
+# pokemon characteristics
+res<-data.frame(pokemon %>% dplyr::select(Type.1,HP, Attack, Defense, Sp..Atk, Sp..Def, Speed) %>% dplyr::group_by(Type.1) %>% dplyr::summarise_all(funs(mean)) %>% mutate(sumChars = HP + Attack + Defense + Sp..Atk + Sp..Def + Speed) %>% arrange(-sumChars))
+res$color<-color
+max<- ceiling(apply(res[,2:7], 2, function(x) max(x, na.rm = TRUE)) %>% sapply(as.double)) %>% as.vector
+min<-rep.int(0,6)
+
+par(mfrow=c(3,6))
+par(mar=c(1,1,1,1))
+for(i in 1:nrow(res)){
+  curCol<-(col2rgb(as.character(res$color[i]))%>% as.integer())/255
+  radarchart(rbind(max,min,res[i,2:7]),
+             axistype=2 , 
+             pcol=rgb(curCol[1],curCol[2],curCol[3], alpha = 1) ,
+             pfcol=rgb(curCol[1],curCol[2],curCol[3],.5) ,
+             plwd=2 , cglcol="grey", cglty=1, 
+             axislabcol="black", caxislabels=seq(0,2000,5), cglwd=0.8, vlcex=0.8,
+             title=as.character(res$Type.1[i]))
+}
+names <- pokemon[,c(1,2)]; head(names) # join id with pokemon name
+colnames(names)
 head(pokemon)
-colnames(pokemon)
 
-dim(fights)
-head(fights)
-colnames(fights)
+##### DATA MANIPULATION #####
 
 names <- pokemon[,c(1,2)]; head(names) # join id with pokemon name
 colnames(names)
