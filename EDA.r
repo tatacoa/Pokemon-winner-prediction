1da3d10b6dcb0d7755c53485740df0f71a3c1b93
agademic <arthur.gogohia@gmail.com>
2019-01-11 15:19:11 +0100

    updated

--- EDA.r
+++ EDA.r
@@ -20,7 +20,6 @@ library(rpart.plot)
 library(ROCR)
 #install.packages('tidyverse')
 #library(tidyverse)
-library(ggplot2)
 
 ##### DATA IMPORT #####
 # Load data
@@ -33,6 +32,7 @@ dim(pokemon)
 names(pokemon)
 head(pokemon)
 colSums(is.na(pokemon)) # no missing values
+table(pokemon$Type.1)
 summary(pokemon)
 
 dim(fights)
@@ -43,6 +43,11 @@ wins <- group_by(fights, Winner)
 summarise(wins, count = n())
 
 # visualization
+# feature correlation
+featCorr <- cor(select(pokemon, HP, Attack, Defense, Sp..Atk, Sp..Def, Speed )) # correlation table for features
+corrplot(featCorr, method = "color", type = "upper", addCoef.col = "black", tl.col = "black", diag = FALSE)
+
+# pokemon type distribution
 par(las=2)
 barplot(table(pokemon$Type.1), 
         col = heat.colors(length(unique(pokemon$Type.1))), 
@@ -82,19 +87,24 @@ boxplot(Defense ~ Type.1,
         border="brown"
 )
 
-# set color for each pokemon type for plotting
+# set color for each pokemon type for plotting // hex codes from http://www.epidemicjohto.com/t882-type-colors-hex-colors
 color<-c("#6F35FC","#B7B7CE","#A98FF3","#F95587","#B6A136","#EE8130","#F7D02C","#705746","#735797","#E2BF65","#96D9D6","#6390F0","#7AC74C","#C22E28","#D685AD","#A33EA1","#A8A77A","#A6B91A")
 
 # pokemon characteristics
-res<-data.frame(pokemon %>% dplyr::select(Type.1,HP, Attack, Defense, Sp..Atk, Sp..Def, Speed) %>% dplyr::group_by(Type.1) %>% dplyr::summarise_all(funs(mean)) %>% mutate(sumChars = HP + Attack + Defense + Sp..Atk + Sp..Def + Speed) %>% arrange(-sumChars))
-res$color<-color
+#res<-data.frame(pokemon %>% dplyr::select(Type.1,HP, Attack, Defense, Sp..Atk, Sp..Def, Speed) %>% dplyr::group_by(Type.1) %>% dplyr::summarise_all(funs(mean)) %>% mutate(sumChars = HP + Attack + Defense + Sp..Atk + Sp..Def + Speed) %>% arrange(-sumChars))
+res <- select(pokemon, Type.1, HP, Attack, Defense, Sp..Atk, Sp..Def, Speed) # select particular features
+res <- group_by(res, Type.1) # group by pokemon type
+res <- summarise_all(res, funs(mean)) # get mean values for the types
+res <- mutate(res, sumChars = HP + Attack + Defense + Sp..Atk + Sp..Def + Speed) # sum up all mean values
+res <- arrange(res, -sumChars) # sort for values, descending
+res$color<-color # apply color scheme
 max<- ceiling(apply(res[,2:7], 2, function(x) max(x, na.rm = TRUE)) %>% sapply(as.double)) %>% as.vector
 min<-rep.int(0,6)
 
 par(mfrow=c(3,6))
 par(mar=c(1,1,1,1))
 for(i in 1:nrow(res)){
-  curCol<-(col2rgb(as.character(res$color[i]))%>% as.integer())/255
+  curCol<-(col2rgb(as.character(res$color[i]))%>% as.integer())/255 # convert to rgb
   radarchart(rbind(max,min,res[i,2:7]),
              axistype=2 , 
              pcol=rgb(curCol[1],curCol[2],curCol[3], alpha = 1) ,
